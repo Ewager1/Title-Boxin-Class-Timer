4bfbb7535e407b3cfe0615cee7a7a335bb25acb8
Ethan <ethanbwager@gmail.com>
2021-06-09 17:00:40 -0400

    changed serviceworker function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import './index.scss';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import "./index.scss";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-serviceWorkerRegistration.unregister();
-
+serviceWorkerRegistration.register();
